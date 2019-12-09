fdf739eacd4942a54dda5c6f985ef6b120151c67
acepop <acecool11gaming@gmail.com>
2019-12-09 00:20:38 +0000

    added a unflinching variability

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+
+
+
+
+
 class Main {
+
+
+
+  
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    System.out.println("Hello bob");
+    private int one;
+
+    System.out.println(one);
     System.out.println("i am jan");
   }
 }
