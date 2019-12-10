e94aa06f78ec3842a4040d989bad3943bb45e4a7
acepop <acecool11gaming@gmail.com>
2019-12-10 18:19:07 +0000

    fixed program

--- Main.java
+++ Main.java
@@ -1,18 +1,28 @@
-
+import java.util.Scanner;
 
 
 
 
 class Main {
-
+  int lol = 69;
+public static void wow(){
+  Scanner sc = new Scanner(System.in);
+    System.out.println("Added + 1");
+    String wow = sc.nextLine();
+    System.out.println("type show");
+    if (wow.equals("show")){
+      System.out.println("This scanner is how you ask for user input");
+    }
+}
 
 
   
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    private int one;
+    int one = 1;
 
     System.out.println(one);
     System.out.println("i am jan");
+    wow();
   }
 }
